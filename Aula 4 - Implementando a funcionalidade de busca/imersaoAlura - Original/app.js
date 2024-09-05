e65a5ff914d2cc9a29fc5997a3fb09d7f9056a9e
Wallace Virginio <77004273+wallace-virginio@users.noreply.github.com>
2024-09-05 16:45:32 -0300

    Adicionando arquivos do projeto + Aulas

---
+++ Aula 4 - Implementando a funcionalidade de busca/imersaoAlura - Original/app.js
@@ -0,0 +1,32 @@
+function pesquisar() {
+    // Função é acionada quando o botão de pesquisa é clicado
+    console.log("clicou! :)"); // Mensagem para confirmar que a função foi executada
+    // Comentário para lembrar que 'dados' é uma variável global ou já definida anteriormente
+    // console.log(dados); // Linha comentada, provavelmente usada para debug
+
+    // Seleciona a seção HTML com o ID 'resultados-pesquisa' para inserir os resultados
+    let section = document.getElementById('resultados-pesquisa');
+    console.log(section); // Verifica se a seção foi encontrada
+
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+
+    // Itera sobre cada elemento 'dado' dentro da lista de dados
+    for (let dado of dados) {
+        // Cria uma nova div para cada resultado, formatando os dados
+        resultados += `
+    <div class="item-resultado">
+      <h2>
+        <a href="#" target="_blank">${dado.descricao}</a>
+      </h2>
+      <p class="descricao-meta">${dado.titulo}</p>
+      <a href="${dado.link}" target="_blank">Mais informações</a>
+    </div>
+  `;
+    }
+
+    // Insere os resultados formatados dentro da seção HTML selecionada
+    section.innerHTML = resultados;
+}
+
+
---
+++ Aula 4 - Implementando a funcionalidade de busca/imersaoAlura - Original/dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo: " Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma.Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        descricao: " Rayssa Leal",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+    },
+    {
+        titulo: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, sendo considerada uma das maiores ginastas da história do Brasil.",
+        descricao: "Rebeca Andrade",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+    },
+
+    {
+        titulo: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos, conquistando a medalha de ouro e se tornando um dos principais nomes do judô brasileiro.",
+        descricao: "Beatriz Souza",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+    }
+];
+
+
+
